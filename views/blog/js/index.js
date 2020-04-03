a361cf7cdee51ca913fd608c637dde108be9e663
张云 <zhy521up@163.com>
2020-04-03 15:48:31 +0800

    blog

--- views/blog/js/index.js
+++ views/blog/js/index.js
@@ -1,8 +1,10 @@
 sessionStorage.removeItem("CLOUDEMPEROR_TOKEN")
 
+
 function login(){
     var value = document.getElementById('password').value;
     var ps = window.btoa('CloudEmperor='+ value)
+    console.log(ps)
     switch (ps){
         //CurriculumVitae page
         case 'Q2xvdWRFbXBlcm9yPXpoeTU1NDk3MTM3NQ==' : 
@@ -12,6 +14,18 @@ function login(){
         //Image Watermarking page
         case 'Q2xvdWRFbXBlcm9yPWltZw==':
             window.location.href = 'https://cloudemperor.github.io/blog/views/watermarking/index.html'
+            break;
+        // Github Technology blog
+        case 'Q2xvdWRFbXBlcm9yPWdpdGh1Yg==' :
+            window.location.href = 'https://github.com/CloudEmperor/blog'
+            break;
+        // juejin Technology blog
+        case 'Q2xvdWRFbXBlcm9yPWp1ZWppbg==':
+            window.location.href = 'https://juejin.im/user/5b7c1540f265da4355387634'
+            break;
+        // CSDN Technology blog
+        case 'Q2xvdWRFbXBlcm9yPWNzZG4=':
+            window.location.href = 'https://me.csdn.net/weixin_38008863'
             break;
         default :
             alert('口令或者密码输入有误')
